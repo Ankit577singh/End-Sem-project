86cd168cbaa3f9e01671db1e3319625a9dc8cd59
Arjun Mishra <arjunmishraaa01@gmail.com>
2025-08-03 23:12:43 +0530

    Adding  Footer.jsx

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -2,7 +2,7 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React from "react";
 import "./Footer.scss";
 
-function Footer({ home_page }) {
+function Footer({ home_page }) { //check this want i done
    return (
       <div className="footer">
          <div className="footer__content">
